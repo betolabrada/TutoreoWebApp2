21cb9fafaa97cedfd9403adcb4df2a9dc86c4454
Alberto Labrada <31485362+betolabrada@users.noreply.github.com>
2019-11-10 16:05:52 -0600

    added components, fixing modules

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+
+import 'bootstrap'
+import {library} from '@fortawesome/fontawesome-svg-core'
+
+import "bootstrap/dist/css/bootstrap.css"
+import "animate.css/animate.css"
+
+import {
+  faEdit
+} from "@fortawesome/free-solid-svg-icons"
+
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+library.add(faEdit);
+
+Vue.use(VueMaterial);
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/router.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from './components/Home'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/signup',
+      name: 'SignUp',
+      component: SignUp
+    }
+  ]
+})
