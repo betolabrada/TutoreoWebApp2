86acf8b7abc0153486145bd943f89ac30a862240
Alberto Labrada <31485362+betolabrada@users.noreply.github.com>
2019-11-21 17:53:03 -0600

    sdf

--- src/components/DashboardMateria.vue
+++ src/components/DashboardMateria.vue
@@ -1,41 +1,53 @@
 <template>
-	<div class="container">
-  	<router-link class="btn btn-secondary" to="/Dashboard">Dashboard</router-link>
-  	<h1>{{name}}</h1>
-  	<div class="col-xs-6">
-<h2 class="sub-header">Contenido</h2>
-  <div class="table-responsive">
-            <table class="table table-striped">
-              <tbody>
-                <tr><!--repetir con v-for -->
-                  <td class="col-md-1"><router-link class="btn btn-secondary" :to="{ name: 'DetalleContenido', params: {id_curso: id_curso, id_contenido: contenidos[0].id_contenido} } " >nombre del contenido</router-link></td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-</div>
-  <div class="col-xs-6">
-          <h2 class="sub-header">Tareas</h2>
-          <div class="table-responsive">
-            <table class="table table-striped">
-              <thead>
-                <tr>
-                  <th class="col-md-1">#</th>
-                  <th class="col-md-2">Calificacion</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr><!--repetir con v-for -->
-                  <td class="col-md-1"><router-link class="btn btn-secondary" :to="{ name: 'DetalleTarea', params: {id_curso: id_curso, id_tarea: tareas[0].id_tarea} } " >nombre de la tarea</router-link></td>
-                  <td class="col-md-1">nota</td>
-                </tr>
-              </tbody>
-            </table></div>
+  <div class="container">
+    <router-link class="btn btn-secondary" to="/Dashboard">Dashboard</router-link>
+    <h1>{{name}}</h1>
+    <div class="col-xs-6">
+      <h2 class="sub-header">Contenido</h2>
+      <div class="table-responsive">
+        <table class="table table-striped">
+          <tbody>
+            <tr>
+              <!--repetir con v-for -->
+              <td class="col-md-1">
+                <router-link
+                  class="btn btn-secondary"
+                  :to="{ name: 'DetalleContenido', params: {id_curso: id_curso, id_contenido: contenidos[0].id_contenido} } "
+                >nombre del contenido</router-link>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="col-xs-6">
+      <h2 class="sub-header">Tareas</h2>
+      <div class="table-responsive">
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th class="col-md-1">#</th>
+              <th class="col-md-2">Calificacion</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <!--repetir con v-for -->
+              <td class="col-md-1">
+                <router-link
+                  class="btn btn-secondary"
+                  :to="{ name: 'DetalleTarea', params: {id_curso: id_curso, id_tarea: tareas[0].id_tarea} } "
+                >nombre de la tarea</router-link>
+              </td>
+              <td class="col-md-1">nota</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
-
-            <div id="push"></div>
-        </div>
-	</div>
+      <div id="push"></div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -43,12 +55,12 @@ import db from "@/db.js";
 export default {
   name: "DashboardMateria",
   data: function() {
-  	return {
-  		id_curso: '',
-  		name: '',
-  		tareas: [],
-  		contenidos: []
-  	}
+    return {
+      id_curso: "",
+      name: "",
+      tareas: [],
+      contenidos: []
+    };
   },
   beforeRouteEnter(to, from, next) {
     db.collection("cursos")
@@ -76,13 +88,12 @@ export default {
       .then(querySnapshot => {
         querySnapshot.forEach(doc => {
           const contenidoData = {
-          	id_contenido: doc.id,
-          	nombre: doc.data().nombre
-          	}
+            id_contenido: doc.id,
+            nombre: doc.data().nombre
+          };
           this.contenidos.push(contenidoData);
         });
       });
-
   }
-}
+};
 </script>
