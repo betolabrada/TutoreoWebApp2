5484b6842e9d49a4264868bffb04ae066450a46c
Alberto Labrada <31485362+betolabrada@users.noreply.github.com>
2019-11-21 21:02:42 -0600

    cambios de tareacontenido

--- src/components/BootstrapModalInput.vue
+++ src/components/BootstrapModalInput.vue
@@ -6,19 +6,41 @@
           <div class="modal-dialog" role="document">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title">Nuevo {{titulo}}</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true" @click="$emit('close', false)">&times;</span>
-                </button>
+                <h5 class="modal-title">Nuevo {{tipo}}</h5>
               </div>
               <div class="modal-body">
-                <simple-input type="text" id="titulo" placeholder="ingresa un titulo para el {{titulo}}"  />
-                <simple-input type="text" id="titulo" placeholder="ingresa una descripcion para el {{titulo}}"  />
-                <simple-text-area type="text" id="titulo" placeholder="ingresa una descripcion para el {{titulo}}"  />
+                <div class="container">
+                  <div class="col-10 form-group">
+                    <simple-input
+                      type="text"
+                      id="titulo"
+                      :placeholder="'ingresa un titulo para: ' + tipo"
+                      v-model="dataToAdd.titulo"
+                    />
+                  </div>
+                </div>
+                <div class="container">
+                  <div class="col-10 form-group">
+                    <simple-text-area
+                      type="text"
+                      id="titulo"
+                      :placeholder="'Escribe  descripcion de: ' + tipo"
+                      v-model="dataToAdd.descripcion"
+                    />
+                  </div>
+                </div>
               </div>
               <div class="modal-footer">
-                <button type="button" class="btn btn-secondary"  @click="$emit('close', false)">cancelar</button>
-                <button type="button" class="btn btn-primary" @click="$emit('confirmed', true)">registrarme</button>
+                <button
+                  type="button"
+                  class="btn btn-primary"
+                  @click="ingresado"
+                >agregar</button>
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  @click="$emit('close', false)"
+                >cancelar</button>
               </div>
             </div>
           </div>
@@ -32,18 +54,32 @@
 import SimpleInput from "@/components/SimpleInput.vue";
 import SimpleTextArea from "@/components/SimpleTextArea.vue";
 export default {
-  data: {
-
+  data:function() {
+    return {
+      dataToAdd: {
+        tipo: "",
+        titulo: "",
+        descripcion: ""
+      }
+    }
   },
+    
   props: {
-    titulo: String,
-  },
-  data: function() {
-    return {};
+    tipo: String
   },
   components: {
     SimpleInput,
     SimpleTextArea
+  },
+  methods: {
+    ingresado: function() {
+      if (this.tipo == "contenido") {
+        this.$emit('contenidoIngresado', this.dataToAdd);
+      }
+      else {
+        this.$emit('tareaIngresada', this.dataToAdd);
+      }
+    }
   }
 };
 </script>
--- src/components/DashboardMateria.vue
+++ src/components/DashboardMateria.vue
@@ -2,8 +2,18 @@
   <div class="container">
     <router-link class="btn btn-secondary mt-2" to="/Dashboard">Dashboard</router-link>
     <div v-if="$route.params.vista_maestro" class="wrapper control">
-      <button @click="subirTarea" class="btn btn-primary ml-3 mt-2">Nueva Tarea</button>
-      <button @click="subirContenido" class="btn btn-primary ml-3 mt-2">Nuevo Contenido</button>
+      <button
+        @click="showModal = true; tipo='tarea'"
+        data-toggle="modal"
+        data-target="#tareaModal"
+        class="btn btn-primary ml-3 mt-2"
+      >Nueva Tarea</button>
+      <button
+        @click="showModal = true; tipo='contenido'"
+        data-toggle="modal"
+        data-target="#contenidoModal"
+        class="btn btn-primary ml-3 mt-2"
+      >Nuevo Contenido</button>
     </div>
     <h1>{{name}}</h1>
     <h2>Profesor: {{maestro}}</h2>
@@ -12,7 +22,7 @@
       <div class="table-responsive">
         <table class="table table-striped">
           <tbody>
-            <tr :key="contenido.id_contenido" v-for="contenido in contenidos" >
+            <tr :key="contenido.id_contenido" v-for="contenido in contenidos">
               <!--repetir con v-for -->
               <td class="col-md-1">
                 <router-link
@@ -39,15 +49,18 @@
             <tr v-if="!tareas">De momento no hay tareas disponibles para este curso</tr>
             <tr :key="tarea.id_tarea" v-for="tarea in tareas">
               <!--repetir con v-for -->
-              <td  class="col-md-1">
+              <td class="col-md-1">
                 <router-link
                   class="btn btn-secondary"
                   :to="{ name: 'DetalleTarea', params: {id_curso: id_curso, id_tarea: tarea.id_tarea} } "
-                >{{tarea.nombre}}</router-link>
+                >{{tarea.titulo}}</router-link>
               </td>
               <td class="col-md-1">nota</td>
               <td class="col-md-1" v-if="!$route.params.vista_maestro">
-                <router-link class="btn btn-info" :to="{ name: 'SubirTarea', params: {id_curso: id_curso, id_tarea: tarea.id_tarea}}">subir tarea</router-link>
+                <router-link
+                  class="btn btn-info"
+                  :to="{ name: 'SubirTarea', params: {id_curso: id_curso, id_tarea: tarea.id_tarea}}"
+                >subir tarea</router-link>
               </td>
             </tr>
           </tbody>
@@ -55,12 +68,22 @@
       </div>
 
       <div id="push"></div>
+      <bootstrap-modal-input
+        id="contenidoModal"
+        v-if="showModal"
+        @close="showModal=false"
+        @contenidoIngresado="subirContenido"
+        @tareaIngresada="subirTarea"
+        :tipo="tipo"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import firebase from "firebase";
 import db from "@/db.js";
+import BootstrapModalInput from "@/components/BootstrapModalInput.vue";
 export default {
   name: "DashboardMateria",
   data: function() {
@@ -70,7 +93,12 @@ export default {
       tareas: [],
       contenidos: [],
       maestro: "",
+      showModal: false,
+      tipo: ""
     };
+  },
+  components: {
+    BootstrapModalInput
   },
   beforeRouteEnter(to, from, next) {
     db.collection("cursos")
@@ -88,6 +116,19 @@ export default {
       });
   },
   mounted() {
+    db.collection("cursos")
+      .where("id_curso", "==", this.$route.params.id_curso)
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          if (doc.exists) {
+            this.id_curso = doc.data().id_curso;
+            this.name = doc.data().nombre;
+            this.contenidos = doc.data().contenidos;
+            this.tareas = doc.data().tareas;
+          }
+        });
+      });
 
     db.collection("maestros")
       .where("curso.id_curso", "==", this.$route.params.id_curso)
@@ -99,11 +140,47 @@ export default {
       });
   },
   methods: {
-    subirTarea: function() {
-      alert("se ha subido la tarea");
+    subirContenido: function(dataToAdd) {
+      const contenido = {
+        titulo: dataToAdd.titulo,
+        descripcion: dataToAdd.descripcion
+      };
+      db.collection("cursos")
+        .doc(this.$route.params.id_curso)
+        .update({
+          contenidos: firebase.firestore.FieldValue.arrayUnion(contenido)
+        })
+        .then(() => {
+          alert("materal subido exitosamente a sistema");
+          this.$router.push({
+            name: "DashboardMateria",
+            params: {
+              id_curso: this.$route.params.id_curso,
+              vista_maestro: true
+            }
+          });
+        });
     },
-    subirContenido: function() {
-      alert("se ha subido el contenido");
+    subirTarea: function(dataToAdd) {
+      const tarea = {
+        titulo: dataToAdd.titulo,
+        descripcion: dataToAdd.descripcion
+      };
+      db.collection("cursos")
+        .doc(this.$route.params.id_curso)
+        .update({
+          tareas: firebase.firestore.FieldValue.arrayUnion(tarea)
+        })
+        .then(() => {
+          alert("tarea subida exitosamente a sistema");
+          this.$router.push({
+            name: "DashboardMateria",
+            params: {
+              id_curso: this.$route.params.id_curso,
+              vista_maestro: true
+            }
+          });
+        });
     },
     subirTareaAlumno: function() {
       alert("Se subirá tarea");
